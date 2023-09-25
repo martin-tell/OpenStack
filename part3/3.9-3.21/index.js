bfbb549469cb90399a9e818e224a99279a61360d
martin-tell <martin.tello.lpz@gmail.com>
2023-09-25 17:02:23 -0600

    ejercicio eslint

--- part3/3.9-3.21/index.js
+++ part3/3.9-3.21/index.js
@@ -10,16 +10,16 @@ app.use(cors()) //This middleware allows other domains to use the resources of t
 app.use(express.json()) //JSON request body will be turned into JS objects, POST and PUT requests use this middleware
 
 app.use(morgan(function (tokens, req, res) {
-    const requestBody = JSON.stringify(req.body);
+    const requestBody = JSON.stringify(req.body)
     return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.req(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      requestBody
-    ].join(' ');
-}));
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.req(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        requestBody
+    ].join(' ')
+}))
 
 app.use(express.static('build')) //React build code will be the main page, build folder uses this middleware
 
@@ -46,20 +46,20 @@ app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
         .then(person => {
             if(person){
-                res.json(person);
+                res.json(person)
             }else{
-                res.status(404).end();
+                res.status(404).end()
             }
         })
-        .catch(error => next(error));
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
         .then(result => {
-            res.status(204).end();
+            res.status(204).end()
         })
-        .catch(error => next(error));
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
@@ -68,7 +68,7 @@ app.post('/api/persons', (req, res, next) => {
     const person = new Person({
         name: body.name,
         number: body.number
-    });
+    })
 
     person.save()
         .then(savedPerson => {
@@ -87,34 +87,34 @@ app.put('/api/persons/:id', (req, res, next) => {
 
     Person.findOneAndUpdate({ _id: req.params.id }, person, {new: true})
         .then(updatedPerson => {
-            res.json(updatedPerson);
+            res.json(updatedPerson)
         })
         .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'});
+    response.status(404).send({error: 'unknown endpoint'})
 }
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message);
+    console.log(error.message)
 
     if(error.name === 'CastError'){
-        return response.status(400).send({error: 'malformatted id'});
+        return response.status(400).send({error: 'malformatted id'})
     } else if (error.name === 'ValidationError'){
         return response.status(400).json({ error: error.message })
     }
 
-    next(error);
+    next(error)
 }
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`)
 })
 
 //Me quede Ejercicios 3.15.-3.18. solo afinar detalles del encabezado /info no carga desde el navegador
